c73812a185a79ea14fa0357760891271941d4050
zyroskai <zyroskai@gmail.com>
2025-05-11 01:24:30 +0100

    Update test.js

--- MY_HTML_2/test.js
+++ MY_HTML_2/test.js
@@ -6,8 +6,8 @@ name:'kailash',
 age:'20',
 fun : function tem(){
    console.log('test '+ this.name);
-                     }
-             }
+ }
+ }
    console.log(sum.tem);
 
 /*Arrow funtion in array*/
@@ -22,3 +22,13 @@ console.log(arrays.map( (tem)=>{ return tem+' Hello'}))
 const arrays1 = ["apple","ball"];
 
 console.log(arrays1.map( tem => tem+' Hello'))
+
+
+/*Copy array*/
+const arrays = ['apple','ball']
+const arrays1=  arrays.slice()
+
+arrays.push('cat')
+
+console.log(arrays1);
+console.log(arrays);
